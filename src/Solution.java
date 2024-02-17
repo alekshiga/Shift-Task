389407698a8082a4c90500a44468c1ea37c002bd
Sergey <alekshiga@vk.com>
2024-02-18 01:18:34 +0700

    return if no input files

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Solution {
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.out.print("Не найдено входных файлов");
+            return;
+        }
+
+        for (String filename : args) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+                String line;
+
+            } catch (IOException e) {
+                System.err.println("Ошибка чтения файла " + filename + ": " + e.getMessage());
+            }
+        }
+
+        System.out.println("Данные файлов отсортированы");
+    }
 }
